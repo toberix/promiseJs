3cde3a9b7500653afaba0fc203e2dc04f3b8b17e
Tobi Fatumo <tfatumo@emzorlan.com>
2018-05-16 19:46:22 +0100

    using arrow function

--- promise.js
+++ promise.js
@@ -12,43 +12,29 @@ let promiseToCleanTheRoom = new Promise(function(resolve, reject){
 
 
 
-promiseToCleanTheRoom.then(function(fromResolve){
-  console.log('The Room ' + fromResolve)
-}).catch(function(fromReject){
-  console.log('The Room ' + fromReject)
-});
-
-let cleanRoom = function() {
+let cleanRoom = () => {
   return new Promise(function(resolve, reject){
     resolve('Cleaned The Room');
   });
 };
 
-let removeGarbage = function(message){
+let removeGarbage = (message) =>{
   return new Promise(function(resolve, reject){
     resolve(message + ' remove Garbage');
   });
 };
 
-let winIcecream = function(message){
+let winIcecream = (message) =>{
   return new Promise(function(resolve, reject){
     resolve(message + ' won Icecream');
   });
 };
 
-cleanRoom().then(function(){
-  return removeGarbage();
-}).then(function(){
-  return winIcecream()
-}).then(function)
 
-
-// USING ARROW FUNCTION
-
-// cleanRoom().then((resul)=>{
-//   return removeGarbage(resul);
-// }).then((resul) => {
-//   return winIcecream()
-// }).then((resul)=>{
-//   console.log('finished ' + resul)
-// })
+cleanRoom().then((output)=>{
+  return removeGarbage(output);
+}).then((output) => {
+  return winIcecream(output)
+}).then((output)=>{
+  console.log('finished ' + output)
+})
