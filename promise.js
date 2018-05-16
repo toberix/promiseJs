010f91b4472eaca39d9fba0f4b8b0c9ec667ea7d
Tobi Fatumo <tfatumo@emzorlan.com>
2018-05-16 19:41:12 +0100

    more option

--- promise.js
+++ promise.js
@@ -10,8 +10,45 @@ let promiseToCleanTheRoom = new Promise(function(resolve, reject){
     }
 });
 
+
+
 promiseToCleanTheRoom.then(function(fromResolve){
   console.log('The Room ' + fromResolve)
 }).catch(function(fromReject){
   console.log('The Room ' + fromReject)
 });
+
+let cleanRoom = function() {
+  return new Promise(function(resolve, reject){
+    resolve('Cleaned The Room');
+  });
+};
+
+let removeGarbage = function(message){
+  return new Promise(function(resolve, reject){
+    resolve(message + ' remove Garbage');
+  });
+};
+
+let winIcecream = function(message){
+  return new Promise(function(resolve, reject){
+    resolve(message + ' won Icecream');
+  });
+};
+
+cleanRoom().then(function(){
+  return removeGarbage();
+}).then(function(){
+  return winIcecream()
+}).then(function)
+
+
+// USING ARROW FUNCTION
+
+// cleanRoom().then((resul)=>{
+//   return removeGarbage(resul);
+// }).then((resul) => {
+//   return winIcecream()
+// }).then((resul)=>{
+//   console.log('finished ' + resul)
+// })
